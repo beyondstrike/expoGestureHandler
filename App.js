ed9842636bea0180a30d01edbd9e33a86872da1b
Kareem Zahran <karemzh@live.com>
2022-02-01 19:54:21 +0200

    React native expo gestuer handler

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { PanGestureHandler } from "react-native-gesture-handler";
+import "react-native-gesture-handler";
+import Animated, {
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+} from "react-native-reanimated";
+
+import { StyleSheet, View, Text } from "react-native";
+
+const SIZE = 100.0;
 
 export default function App() {
+  const translateX = useSharedValue(0);
+  const panGestureEvent = useAnimatedGestureHandler({
+    onStart: (event) => {
+      console.log("starting...");
+    },
+    onActive: (event) => {
+      // translateX.value = event.translationX;
+      console.log(event.translationX);
+    },
+    onEnd: (event) => {},
+  });
+
+  const rStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: translateX.value }],
+    };
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <View>
+          <Text>{translateX.value}</Text>
+        </View>
+        <PanGestureHandler onGestureEvent={panGestureEvent}>
+          <Animated.View style={[styles.square, rStyle]} />
+        </PanGestureHandler>
+      </View>
     </View>
   );
 }
@@ -13,8 +46,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  square: {
+    width: SIZE,
+    height: SIZE,
+    backgroundColor: "dodgerblue",
+    borderRadius: 15,
   },
 });
